193370b9efb133958907fdb7543ec84163c4853a
Brent Zerbe <zerbzman@gmail.com>
2023-05-28 20:08:17 -0400

    Initial support a success

--- paradigm.js
+++ paradigm.js
@@ -4,27 +4,28 @@ class Paradigm {
 		this.host = host
 		// this.port = 502
 		this.connection = undefined
-		// this.controller = {
-		// 	info: {},
-		// 	panels: [],
-		// 	buses: [],
-		// }
+		this.system = {}
+		this.macros = []
+		this.walls = []
+        this.spaces = []
+        this.overrides = []
+        this.channels = []
+        this.sequences = []
+        this.url = `http://${this.host}/get/`
 		// this.buildBuses()
 	}
 
-	async init() {
-		this.connection = await connect(this.host)
-	}
-
-    async connect() {
-        const url = `http://${this.host}/get/system`
-
+    async getInfo(info) {
+        const url = this.url + info
+        const options = {}
         let response
+
         try {
             response = await fetch(url, options)
             if (response.status === 200) {
-                const result = await response.json();
-                console.log('result:',result);
+                const result = await response.text();
+                const filtered = result.replace(/,(?=[\]}])/g, '')
+                this[info] = JSON.parse(filtered)
               } else {
                 throw new Error("Something went wrong on API server!");
               }
@@ -33,59 +34,29 @@ class Paradigm {
         }
     }
 
-    async getMacros() {
-        if (this.connection === undefined) {
-            return false
-        }
-
-        const url = `http://${this.host}/get/macros`
-
-        let response
+    async connect() {
+        // get system info
         try {
-            response = await fetch(url, options)
-        } catch (error) {
+            await this.getInfo('system')
+            await this.getInfo('walls')
+            await this.getInfo('macros')
+            await this.getInfo('spaces')
+            await this.getInfo('overrides')
+            await this.getInfo('channels')
+            await this.getInfo('sequences')
+
+        // this.connection =
+        } catch(error) {
             console.log(error);
         }
+        
+        console.log(this)
+        return this
     }
 
-    /**
-	 * This is a workaround for a fetch request because Node 18 has a bug and can't
-	 * process the headers correctly.
-	 * @param  {url} url               The URL to request
-	 * @param  {JSON Object} body      The POST Body
-	 * @return {string}      The response as a string
-	 */
-	// makeRequest(url, options = {}) {
-	// 	return new Promise((resolve, reject) => {
-	// 		const postData = querystring.stringify(body)
-
-	// 		// const options = {
-	// 		// 	method,
-	// 		// 	headers: {
-	// 		// 		'Content-Type': 'application/javascript',
-	// 		// 		'Content-Length': Buffer.byteLength(postData)
-	// 		// 	},
-	// 		// 	insecureHTTPParser: true
-	// 		// }
-
-	// 		const req = http.request(url, options, (res) => {
-	// 			let data = ''
-	// 			res.on('data', (chunk) => {
-	// 				data += chunk
-	// 			})
-	// 			res.on('end', () => {
-	// 				resolve(data)
-	// 			})
-	// 		})
-
-	// 		req.on('error', (error) => {
-	// 			reject(error)
-	// 		})
-
-	// 		req.write(postData)
-	// 		req.end()
-	// 	})
-	// }
+	async init() {
+		this.connection = await this.connect(this.host)
+	}
 
 	close() {
 		if (this.connection) {
